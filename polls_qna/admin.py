75d7e8283a7e6cfed3dcb90d1951228a7bc0a4bb
Ana Geguchadze <anageguchadze22@gmail.com>
2024-12-11 10:14:29 +0100

    Register models for polls_qna and idea_block in Django admin interface

--- polls_qna/admin.py
+++ polls_qna/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Poll, PollOption, PollResponse, Question, Answer
 
 # Register your models here.
+admin.site.register(Poll)
+admin.site.register(PollOption)
+admin.site.register(PollResponse)
+admin.site.register(Question)
+admin.site.register(Answer)
