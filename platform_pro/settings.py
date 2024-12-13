9255f87f05647a8408e77e58c5a21ae465d64585
Ana Geguchadze <anageguchadze22@gmail.com>
2024-12-13 19:46:51 +0100

    Add swagger documentation

--- platform_pro/settings.py
+++ platform_pro/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+    'drf_yasg',
     'platform_app',
     'push_notifications',
     'idea_block',
